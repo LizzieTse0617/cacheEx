0a959390fe1cf728bd2eb3d3c4dc039f7f726a82
Lizzie Tse <lizzie.tse617@gmail.com>
2023-01-27 22:56:32 -0500

    draft

--- js/app.js
+++ js/app.js
@@ -1,23 +1,29 @@
-import CACHE from './cache.js';
-
+import CACHE from "./cache.js";
 //All the DOM functionality and control of the application happens in this file
 //All the code dealing with the Cache is in the cache.js file.
 const APP = {
   itemList: [],
-  activeLI: '',
+  activeLI: "",
+  // cacheName: "myCache-v1",
   init() {
     //page loaded
-    document.getElementById('itemForm').addEventListener('submit', APP.addItem);
-    document.getElementById('btnItem').addEventListener('click', APP.addItem);
-    document.getElementById('btnList').addEventListener('click', APP.saveListAsFile);
+    document.getElementById("itemForm").addEventListener("submit", APP.addItem);
+    document.getElementById("btnItem").addEventListener("click", APP.addItem);
+    document
+      .getElementById("btnList")
+      .addEventListener("click", APP.saveListAsFile);
     //access the cache
+    CACHE.init("asdhjasd");
     //then display files
     //and then show all the current files
+
+
+
   },
   addItem(ev) {
     //add an item to the list
     ev.preventDefault();
-    let item = document.getElementById('gItem').value;
+    let item = document.getElementById("gItem").value;
     item = item.trim();
     if (!item) return;
     APP.itemList.push(item);
@@ -25,27 +31,41 @@ const APP = {
   },
   displayList() {
     //populate the list of items
-    let list = document.getElementById('item_list');
+    let list = document.getElementById("item_list");
     if (APP.itemList.length === 0) {
-      list.innerHTML = 'No Items currently.';
+      list.innerHTML = "No Items currently.";
     } else {
       list.innerHTML = APP.itemList
         .map((txt) => {
           return `<li>${txt}</li>`;
         })
-        .join('');
+        .join("");
     }
-    document.getElementById('gItem').value = '';
+    document.getElementById("gItem").value = "";
   },
   saveListAsFile(ev) {
     ev.preventDefault();
     //turn the data from the list into the contents for a json file
+    let json = JSON.stringify(APP.itemList);
     //and then create a file with the json
+    let file = new File([json], "isTheJsonFile", {
+      type: "text/plain",
+      lastModified: Date.now(),
+    });
     //and then create a response object to hold the file
+    //let request = new Request(`./data/${APP.itemList}`);
+
+    let request = new Request(`itemlist-${Date.now()}`);
+    let response = new Response(file, {
+      status: 200,
+      statusText: "Ok",
+    });
     //and then save the response in the cache
+    CACHE.open(request, response);
   },
   saveFile(filename, response) {
     //create a url or request object
+    //let request = new Request(`itemlist-${Date.now()}`);
     //save the file in the Cache
     //when file has been saved,
     //clear the displayed list
@@ -58,6 +78,9 @@ const APP = {
   displayFiles(matches) {
     //show the file names from the cache as a list.
     //each list item contains a span for the file name plus a button for deleting the file from the cache
+
+    //displayFileContents(ev);
+//deleteFile(ev);
   },
   displayFileContents(ev) {
     //get the list item from the file
@@ -65,11 +88,12 @@ const APP = {
   },
   deleteFile(ev) {
     ev.preventDefault();
+
+    //*** */cache.delete(reference) + DOM will disappend
     //user has clicked on a button in the file list
     //delete the file from the cache using the file name
     //remove the list item from the list if successful
     //clear the code contents
   },
 };
-
-document.addEventListener('DOMContentLoaded', APP.init);
+document.addEventListener("DOMContentLoaded", APP.init);
--- js/cache.js
+++ js/cache.js
@@ -3,18 +3,45 @@
 // caches.keys()
 // caches.delete()
 // caches.matchAll()
-
 // cache.put()
 // cache.match()
-
 const CACHE = {
   cacheVersion: 1,
   cacheName: null, //this gets set in the init() method
-  userName: 'abcd0001', //replace this with your own username
+  userName: "lizzie0617", //replace this with your own username
   init() {
     //
+    //cacheName = key name for our cache
     CACHE.cacheName = `filecache-${CACHE.userName}-${CACHE.cacheVersion}`;
+    // caches
+    //   .open(CACHE.cacheName)
+    //   .then((cache) => {
+    //     let request = new Request(`./data/${APP.itemList}`);
+    //     let response = new Response(file, {
+    //       status: 200,
+    //       statusText: "Ok",
+    //     });
+    //     cache.put(request, response);
+    //   })
+    //   .catch(console.warn);
   },
-};
+  open(req, res) {
+    caches.open(CACHE.cacheName).then((cache) => {
+      cache.put(req, res);
+    });
+  },
 
+
+
+//cache.delete
+
+
+
+};
 export default CACHE;
+
+
+
+
+
+
